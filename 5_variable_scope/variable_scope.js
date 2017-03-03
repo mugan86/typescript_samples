4395ef70adaf506be15c3b9028f6034cbb6927bc
anartzmugika <mugan86@gmail.com>
2017-03-03 19:44:13 +0100

    Add variables / scope example

---
+++ 5_variable_scope/variable_scope.js
@@ -0,0 +1,15 @@
+var global_num = 12; //global variable
+var Numbers = (function () {
+    function Numbers() {
+        this.num_val = 13; //class variable
+    }
+    Numbers.prototype.storeNum = function () {
+        var local_num = 14; //local variable
+    };
+    return Numbers;
+}());
+Numbers.sval = 10; //static field
+console.log("Global num: " + global_num);
+console.log(Numbers.sval); //static variable
+var obj = new Numbers();
+console.log("Global num: " + obj.num_val);
