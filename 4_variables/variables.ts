80bb087475a84e180b0f0602f48b356ed81a7642
anartzmugika <mugan86@gmail.com>
2017-03-03 19:43:57 +0100

    Add Variables example

---
+++ 4_variables/variables.ts
@@ -0,0 +1,8 @@
+let name:string = "John";
+let score1:number = 50;
+let score2:number = 42.50
+let sum = score1 + score2
+console.log("name: "+name)
+console.log("first score: "+score1)
+console.log("second score: "+score2)
+console.log("sum of the scores: "+sum)
